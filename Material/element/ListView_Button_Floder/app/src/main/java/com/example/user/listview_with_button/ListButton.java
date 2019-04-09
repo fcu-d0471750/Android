cc8b043938c8132e22c0669da9cecfb78bf3aa1d
D0471750 <playwithme401@gmail.com>
2019-04-09 09:48:59 +0800

    Add Material Folder 2019/04/09

--- Material/element/ListView_Button_Floder/app/src/main/java/com/example/user/listview_with_button/ListButton.java
+++ Material/element/ListView_Button_Floder/app/src/main/java/com/example/user/listview_with_button/ListButton.java
@@ -1,3 +1,6 @@
+/*
+*ListView畫面
+* */
 package com.example.user.listview_with_button;
 
 import android.support.v7.app.AppCompatActivity;
@@ -11,32 +14,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListButton extends AppCompatActivity {
-    //ListView控件
+    //ListView元件
     private ListView mList;
-    //ListView数据源
+    //ListView資料
     private List<String> data;
 
+    //=========================================
+    //onCreate
+    //=========================================
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_button);
 
         data = new ArrayList<>();
+        //綁定ListView
         mList = (ListView)findViewById(R.id.mList);
+
+        //加入資料，傳給MyAdapter處理
         for(int i = 0; i < 20; i ++){
-            data.add("今天好手气" + i);
+            data.add("今天好手氣" + i);
         }
+
+        //使用自訂的MyAdapter
         MyAdapter adapter = new MyAdapter(data);
+        //設定使用的MyAdapter
         mList.setAdapter(adapter);
 
-        //ListView item点击事件
+        //ListView item點擊事件
         mList.setOnItemClickListener(new ListView.OnItemClickListener(){
 
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(ListButton.this,"我是item点击事件 i = " + i + "l = " + l,Toast.LENGTH_SHORT).show();
+                Toast.makeText(ListButton.this,"我是item點擊事件 i = " + i + "l = " + l,Toast.LENGTH_SHORT).show();
             }
         });
 
     }
-}
+
+}//ListButton
--- Material/element/ListView_Button_Floder/app/src/main/java/com/example/user/listview_with_button/MyAdapter.java
+++ Material/element/ListView_Button_Floder/app/src/main/java/com/example/user/listview_with_button/MyAdapter.java
@@ -1,3 +1,6 @@
+/*
+* ListView自訂Adapter
+* */
 package com.example.user.listview_with_button;
 
 import android.content.Context;
@@ -14,64 +17,102 @@ import java.util.List;
 
 
 public class MyAdapter extends BaseAdapter implements View.OnClickListener {
+    //======================================
+    //宣告變數
+    //======================================
+    //來自哪個Activity
     private Context context;
+    //用於儲存context傳來的字串
     private List<String> data;
+
+    //儲存從context傳來的字串
     public MyAdapter(List<String> data){
         this.data = data;
     }
+    //data的資料數量
     @Override
     public int getCount() {
         return data == null ? 0 : data.size();
     }
+    //取得data的資料
     @Override
     public Object getItem(int i) {
         return data.get(i);
     }
-
+    //取得data的編號
     @Override
     public long getItemId(int i) {
         return i;
     }
 
+    //======================================
+    //設定ListView(ListView 針對List中每個item，要求 adapter 給一個view，系統會自動將所有傳入的資料都設定ListView，只需處理傳入的資料市要放在哪個元件上)
+    //======================================
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+
+        //宣告ViewHolder類別(未使用)
         ViewHolder viewHolder = null;
+
+        //如果context還沒得到從Activity傳來的資料，則去抓取資料
         if(context == null)
             context = viewGroup.getContext();
+
+        //如果還未設定List的元件，則去設定
         if(view == null){
+            //依照listview_user設定的外觀，設定view的外觀
             view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.listview_user,null);
+            //建立ViewHolder類別(開始使用)
             viewHolder = new ViewHolder();
+            //綁定TextView
             viewHolder.mTv = (TextView)view.findViewById(R.id.mTv);
+            //綁定Button
             viewHolder.mBtn = (Button)view.findViewById(R.id.mBtn);
+            //設定View外觀
             view.setTag(viewHolder);
         }
+
+        //取得所有資料id，也就是編號
         viewHolder = (ViewHolder)view.getTag();
-        //设置tag标记
-        viewHolder.mBtn.setTag(R.id.btn,i);//添加此代码
-        viewHolder.mBtn.setText("点我点我"+ i);
+
+        //設置List元件
+        viewHolder.mBtn.setTag(R.id.btn,i);
+        viewHolder.mBtn.setText("點我"+ i);
         viewHolder.mBtn.setOnClickListener(this);
         viewHolder.mTv.setText(data.get(i));
-        //设置tag标记
-        viewHolder.mTv.setTag(R.id.tv,i);//添加此代码
+
+        //設置tag編號
+        viewHolder.mTv.setTag(R.id.tv,i);
         viewHolder.mTv.setOnClickListener(this);
+
         return view;
     }
+
+    //======================================
+    //按下ListView或Button的事件
+    //======================================
     @Override
     public void onClick(View view) {
+        //依照Id來判斷是ListView或Button
         switch (view.getId()){
             case R.id.mBtn:
                 int b = (int) view.getTag(R.id.btn);
-                Toast.makeText(context,"我是按钮 " + b,Toast.LENGTH_SHORT).show();
+                Toast.makeText(context,"我是按紐 " + b,Toast.LENGTH_SHORT).show();
                 break;
             case R.id.mTv:
                 int t = (int)view.getTag(R.id.tv);
                 Toast.makeText(context,"我是文本" + t,Toast.LENGTH_SHORT).show();
                 break;
-        }
+        }//switch
     }
+
+    //======================================
+    //每個List擁有的元件(一個TextView、一個Button)
+    //======================================
     static class ViewHolder{
         TextView mTv;
         Button mBtn;
     }
-}
+
+}//MyAdapter
 
